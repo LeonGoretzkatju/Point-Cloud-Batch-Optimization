27cc6e227e5adf332c86c794ab3f78dfb3eb9429
LeonGoretzkatju <2755939369@qq.com>
2023-05-11 12:48:40 +0800

    add readme and outlier rejection, but failed

--- FuncUpdate3D.m
+++ FuncUpdate3D.m
@@ -2,15 +2,15 @@ function [Map, Pose] = FuncUpdate3D(Map, Pose, DeltaP, DeltaD)
     DeltaP2 = reshape(DeltaP, 6, [])';
     Pose(2:end, :) = Pose(2:end, :) + DeltaP2;
 
-%     Size_i = Map.Size_i;
-%     Size_j = Map.Size_j;
-%     DeltaD2 = reshape(DeltaD, Size_i, Size_j);
-% 
-%     % Find outliers in DeltaD2 with absolute values larger than 0.08
-%     outliers = abs(DeltaD2) >= 0.05;
-% 
-%     % Set outlier elements to zero
-%     DeltaD2(outliers) = 0;
-% 
-%     Map.Grid = Map.Grid + DeltaD2;
+    Size_i = Map.Size_i;
+    Size_j = Map.Size_j;
+    DeltaD2 = reshape(DeltaD, Size_i, Size_j);
+
+    % Find outliers in DeltaD2 with absolute values larger than 0.08
+    outliers = abs(DeltaD2) >= 0.05;
+
+    % Set outlier elements to zero
+    DeltaD2(outliers) = 0;
+
+    Map.Grid = Map.Grid + DeltaD2;
 end
---
+++ README.md
@@ -0,0 +1,9 @@
+# Direct Batch Optimization for Multiview Registration
+
+## Author: Xiangchen Liu, Liang Zhao
+
+This is the MATLAB code for implementation of our paper "Direct Batch Optimization for Multiview Registration", in this work, we simultaneously optimize robot poses and global 3D grid map, which is totally different from current method which depends on the optimized poses first then use optimized poses to update global map.
+
+### TODO LIST:
+
+Jacobian of observation term w.r.t robot poses $J_p$ is okay, but Jacobian of observation term w.r.t global map $J_D$ has some problem.
