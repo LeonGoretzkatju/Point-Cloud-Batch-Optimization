3c5b68d0a500f4e151171eca7abc67930b15dbce
LeonGoretzkatju <2755939369@qq.com>
2023-05-30 19:27:52 +0800

    change the serious bug in map only update

--- AddNoiseToMap.m
+++ AddNoiseToMap.m
@@ -2,6 +2,6 @@ function Map = AddNoiseToMap(Map)
 Grid = Map.Grid;
 Size_i = Map.Size_i;
 Size_j = Map.Size_j;
-Grid = Grid + 0.5*rand(Size_i,Size_j);
+Grid = Grid + 0.01*rand(Size_i,Size_j);
 Map.Grid = Grid;
 end
